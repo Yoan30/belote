52d71d75239a9eb5057a19edf49b405bc3097a91
Yoan30 <yoan.daude@gmail.com>
2025-09-12 22:58:25 +0200

    Add PWA registration and update game version export

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,5 @@ console.log('Belote Game v1.0.0')
 
 // Main application will be implemented in subsequent commits
 export const GAME_VERSION = '1.0.0'
+import { registerSW } from './pwa/registerSW';
+registerSW();
