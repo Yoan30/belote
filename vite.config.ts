92d5770fad2d6812e08ea42c5ab897f7218d7046
Yoan30 <yoan.daude@gmail.com>
2025-09-14 22:21:15 +0200

    Clarify GitHub Pages comment in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-﻿import { defineConfig } from "vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/belote/",   // important pour GitHub Pages (repo /belote)
+  base: "/belote/", // nécessaire pour GitHub Pages (projet 'belote')
 });
